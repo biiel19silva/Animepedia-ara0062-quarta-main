9f03e1a6c6795be4a07d8a177d508996e5fbc3de
biiel19silva <161843915+biiel19silva@users.noreply.github.com>
2025-10-31 20:36:09 -0300

    Adequação à entrega 3

---
+++ js/carrega_dados.js
@@ -0,0 +1,41 @@
+const tabelaCorpo = document.getElementById('corpo-tabela-animes');
+const urlDados = 'data/catalogo.json';
+
+function carregarCatalogoJSON() {
+
+    fetch(urlDados)
+        .then(response => {
+            if (!response.ok) {
+                throw new Error(`Erro ao buscar dados: ${response.statusText}`);
+            }
+            return response.json();
+        })
+        .then(data => {
+            renderizarAnimes(data);
+        })
+        .catch(error => {
+            console.error('Houve um erro ao carregar o catálogo:', error);
+            tabelaCorpo.innerHTML = `<tr><td colspan="3">Erro ao carregar os filmes.</td></tr>`;
+        });
+}
+
+function renderizarAnimes(animes) {
+
+    let htmlAnimes = '';
+
+    filmes.forEach(anime => {
+        htmlAnimes += ` 
+                    <tr> 
+                        <td><img src="${anime.imagem.src}" alt="${anime.imagem.alt}" width="70"></td> 
+                        <td>${anime.titulo}</td> 
+                        <td>${anime.genero}</td> 
+                    </tr> 
+                `;
+    });
+
+    tabelaCorpo.innerHTML = htmlAnimes;
+
+}
+
+// 4. Inicia o carregamento 
+carregarCatalogoJSON(); 
--- js/validacao.js
+++ js/validacao.js
@@ -10,7 +10,8 @@
 // 4. Um ponto final (.). 
 // 5. Algo no final (o .com, .br, etc.). 
 
-const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/; 
+const emailRegex = /^[a-zA-Z]+\.[a-zA-Z]+@net\.com$/;
+const cpfRegex = /^\d{3}\.\d{3}\.\d{3}-\d{2}$/; 
 
 // =================================== 
 // PARTE 2: A Função de Verificação 
@@ -18,28 +19,58 @@ const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
 
 /** 
  * Objetivo: Checar se o valor no campo de e-mail é válido. 
- * @param {HTMLElement} campo - O campo de texto do e-mail. 
+ * @param {HTMLElement} campoe - O campo de texto do e-mail. 
  * @returns {boolean} - 'true' se for válido, 'false' se não for. 
  */ 
 
-function validarEmail(campo) { 
-    const email = campo.value; 
+function validarEmail(campoe) { 
+    const email = campoe.value; 
 
     // Usamos a "receita" (emailRegex) para testar o e-mail. 
     if (emailRegex.test(email)) { 
         // SE VÁLIDO: 
-        campo.style.border = '1px solid green'; // Borda verde (OK!) 
-        campo.setCustomValidity('');           // Tira qualquer aviso de erro. 
+        campoe.style.border = '1px solid green'; // Borda verde (OK!) 
+        campoe.setCustomValidity('');           // Tira qualquer aviso de erro. 
 
         return true; 
 
     } else { 
         // SE INVÁLIDO: 
-        campo.style.border = '2px solid red';  // Borda vermelha (ERRO!) 
+        campoe.style.border = '2px solid red';  // Borda vermelha (ERRO!) 
 
         // Prepara uma mensagem de erro e a mostra ao aluno: 
         const mensagemErro = 'Por favor, insira um endereço de e-mail válido.'; 
-        campo.setCustomValidity(mensagemErro); 
+        campoe.setCustomValidity(mensagemErro); 
+        //campo.reportValidity(); // Faz o navegador mostrar a caixa de erro. 
+
+        return false; 
+    } 
+} 
+
+/** 
+ * Objetivo: Checar se o valor no campo de cpf é válido. 
+ * @param {HTMLElement} campoc - O campo de texto do cpf. 
+ * @returns {boolean} - 'true' se for válido, 'false' se não for. 
+ */ 
+
+function validarCPF(campoc) { 
+    const cpf = campoc.value; 
+
+    // Usamos a "receita" (emailRegex) para testar o e-mail. 
+    if (cpfRegex.test(cpf)) { 
+        // SE VÁLIDO: 
+        campoc.style.border = '1px solid green'; // Borda verde (OK!) 
+        campoc.setCustomValidity('');           // Tira qualquer aviso de erro. 
+
+        return true; 
+
+    } else { 
+        // SE INVÁLIDO: 
+        campoc.style.border = '2px solid red';  // Borda vermelha (ERRO!) 
+
+        // Prepara uma mensagem de erro e a mostra ao aluno: 
+        const mensagemErro = 'Por favor, insira um endereço de e-mail válido.'; 
+        campoc.setCustomValidity(mensagemErro); 
         //campo.reportValidity(); // Faz o navegador mostrar a caixa de erro. 
 
         return false; 
@@ -56,6 +87,7 @@ document.addEventListener('DOMContentLoaded', function() {
 
     // Acha o campo de e-mail na página pelo ID 'id_email'. 
     const campoEmail = document.getElementById('id_email'); 
+    const campoCPF = document.getElementById('id_cpf');
 
     // --- Ação 1: Checagem Rápida (Saindo do Campo) --- 
     // Sempre que o aluno sai do campo (evento 'blur'), checamos o e-mail. 
@@ -64,4 +96,7 @@ document.addEventListener('DOMContentLoaded', function() {
         validarEmail(campoEmail); 
     }); 
 
-}); 
+campoCPF.addEventListener('blur', function() {
+    validarCPF(campoCPF);
+});
+})
